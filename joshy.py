628ff98886e7d50136aeac2bfa63ed287608697e
Joshua Owen Mangotang <joshuaowen1500@gmail.com>
2025-03-24 11:32:52 +0100

    Committtttttttttttttttt

---
+++ joshy.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+# Joshua Owen Mangotang Joshua Owen Mangotang
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+
+def say_hello(recipient):
+    print("Hello, world! Hello {}".format(recipient))
+    return recipient
